0a550abcaf5302eca02f174e9a6b375163c2101c
Jose Maria Vilaverde <jmvilaverde@gmail.com>
2015-05-20 19:39:42 +0200

    Commit of functional run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,30 +8,101 @@
 ##5.From the data set in step 4, creates a second, independent tidy data set with the average 
 ##of each variable for each activity and each subject.
 
+##Load library dplyr
+library(dplyr)
+
+
+##Main function, calls to 
 run_analysis <- function(){
         
         ##Adquire Data
-        dataSet <- adquireData()
-        fileDestiny
+        adquireFile("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./UCI HAR Dataset")
         
-        ##Merge training and test sets
+        ##Extract data from the files and merge training and test sets in one set
+        dataComplete <- fileToData("./UCI HAR Dataset")
         
+        #Obtains Data set of averages per value grouped by Subject and Activity
+        dataAverage <<- getDataSetAverage(dataComplete)
         
-#directory        
+        #save set to a file
+        saveDataToFile(dataAverage)
 }
 
-adquireData <- function(fileUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"){
+adquireFile <- function(fileUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                        unzipPath = "./UCI HAR Dataset"){
         
         fileZip <- "./temp.zip"
-        download.file(fileUrl, fileZip)
-        unzip(fileZip)
-        file.remove(fileZip)
-        dirPath <- "./UCI HAR Dataset"
-        #unlink(dirPath, recursive = TRUE)
-        testPath <- paste(dirPath,"test", sep = "/")
-        dir(testPath)
-        dataTest <- read.table(paste(testPath,"x_test.txt", sep = "/"))
-        dataTestY <- read.table(paste(testPath,"y_test.txt", sep = "/"))
-        dataTestSub <- read.table(paste(testPath,"subject_test.txt", sep = "/"))
         
+        if (!file.exists(unzipPath)){
+                if (!file.exists(fileZip)){
+                        download.file(fileUrl, fileZip)
+                }
+                unzip(fileZip)
+        }
+        
+        ##Delete directory after process data
+        #unlink(dirPath, recursive = TRUE)
+}
+
+fileToData <- function(path = "./UCI HAR Dataset"){
+        
+        #Get labels for X_???.txt and activity list
+        getLabels(path)
+        
+        testPath <- paste(path,"test/", sep = "/")
+        extTest <- "_test.txt"
+        dataTest <- getDataSet(testPath, extTest)
+        
+        trainPath <- paste(path,"train/", sep = "/")
+        extTrain <- "_train.txt"
+        dataTrain <- getDataSet(trainPath, extTrain)
+        
+        #merge Test and Train set in only one
+        dataComplete <<- rbind(dataTest, dataTrain)
+        dataComplete
+        
+}
+
+
+getLabels <- function(path = "./UCI HAR Dataset"){
+        features <<- read.table(paste(path, "features.txt", sep = "/"))
+        features <<- rename(features, Id = V1, Feature_Label = V2)
+        activity_labels <<- read.table(paste(path, "activity_labels.txt", sep = "/"))
+        activity_labels <<- rename(activity_labels, Id = V1, Activity_Label = V2)
+        
+}
+
+#Obtains data from files, sets the column names, filter columns that only contains "mean" and "std",
+#merges activity Id with his activity name
+#merges all three datasets = X + y + subject
+#return the complete data sets
+
+getDataSet <- function(path, ext){
+        dataX <- read.table(paste(path,"X", ext, sep = ""), col.names = features$Feature_Label)
+        dataY <- read.table(paste(path,"y", ext, sep = ""), col.names = c("Id_Activity"))
+        dataSub <- read.table(paste(path,"subject", ext, sep = ""), col.names = "Subject")
+        
+        #Obtain only data with "mean" or "str" column label for dataX
+        #It's possible to quit "meanFreq", but I consider it as a "mean"
+        dataXOptimized <- dataX[,grepl(pattern="mean", x = names(dataX))|grepl(pattern="std", x = names(dataX))]
+        
+        #Complete data from Y with the name for activities
+        #Inner join between dataY and activity_labels
+        dataYCompleted <<- merge(dataY, activity_labels, by.x="Id_Activity", by.y="Id")
+        
+        data <- cbind(dataXOptimized, dataYCompleted, dataSub)
+        
+        data
+}
+
+getDataSetAverage <- function(dataSet){
+        ##TODO create averagete over grouped_by values
+        data <- aggregate(dataComplete[,1:79], list(dataComplete$Subject, dataComplete$Activity_Label), mean)
+        data <- rename(data, Subject = Group.1, Activity = Group.2)
+        data <- arrange(data, Subject, Activity)
+        data
+}
+
+saveDataToFile <- function(dataToSave){
+        write.table(dataToSave, file="tidyData.txt", row.name=FALSE)
 }
